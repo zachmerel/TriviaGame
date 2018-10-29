6e18f8c73561f82429c32200fe2c7feba4e1ba82
Zach Merel <zachmerel@gmail.com>
2018-10-29 13:41:23 -0500

    title heading added, start button added, timer added but so far just counts down from 30.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
 
-    
+    var timeleft = 30;
+    var questionTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0)
+        clearInterval(questionTimer);
+    },1000);
+
+    $(".start-button").on("click", function () {
+        $(".start-button" ).hide();
+    });
 });
