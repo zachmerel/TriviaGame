d02c46f0f00a2b0ab0c5528c44ee6c68a0dd2e87
Zach Merel <zachmerel@gmail.com>
2018-10-31 15:46:31 -0500

    first question works for right,wrong and unanswered. Does not tally and does not move to next question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,136 +1,169 @@
 $(document).ready(function () {
     //Object of Questions with objects of each question inside
-    var questions = {
-        questionOne : {
+    var questions = [
+        {
+            answered: false,
             text: "What address did Elwood Blues have on his Illinois driver's lisenese?",
             correct: "1060 W Addison St",
             wrongOne: "233 S Wacker Dr",
             wrongTwo: "875 N Michigan Ave",
-            wrongThree: "141 W Jackson Blvd"
+            wrongThree: "141 W Jackson Blvd",
+            image: '<img src="https://media.giphy.com/media/CwMKejp4V6Z1K/giphy.gif"/>'
         },
-        questionTwo : {
+        {
+            answered: false,
             text: "Who was the main villian in the film The Dark Knight?",
             correct: "Joker",
             wrongOne: "Penguin",
             wrongTwo: "Bane",
-            wrongThree: "Scarecrow"
+            wrongThree: "Scarecrow",
+            image: '<img src="https://media.giphy.com/media/KZd26L2o8QXtK/giphy.gif"/>'
         },
-        questionThree : {
+        {
+            answered: false,
             text: "Which is not a place where Ferris and his friends visited while skipping school?",
             correct: "Nay Pier",
             wrongOne: "Wrigley Field",
             wrongTwo: "Von Steuben Day Parade",
-            wrongThree: "Chicago Board of Trade"
+            wrongThree: "Art Institute of Chicago",
+            image: '<img src="https://media.giphy.com/media/3oEjI2hdCm6HODQsdq/giphy.giff"/>'
         },
-        questionFour : {
+        {
+            answered: false,
             text: "Where do Wayne and Garth NOT 'travel' to on the blue screen?",
             correct: "Florida",
             wrongOne: "Hawaii",
             wrongTwo: "Tenxas",
-            wrongThree: "New York"
+            wrongThree: "New York",
+            image: '<img src="https://media.giphy.com/media/cd87BccyY3Ecg/giphy.gif"/>'
         },
-        questionFive : {
+        {
+            answered: false,
             text: "What is Will Smith's character's name in I, Robot?",
             correct: "Del Spooner",
             wrongOne: "Steven Hiller",
             wrongTwo: "Robert Neville",
-            wrongThree: "Mike Lowrey"
+            wrongThree: "Mike Lowrey",
+            image: '<img src="https://media.giphy.com/media/xUOwVmpPRPlosLm5vG/giphy.gif"/>'
         }
-    }
+    ]
+
+    var correct;
+    var incorrect;
+    var unanswered;
+    var currentQuestion;
 
     $(".timerText").hide();
 
 
 
 
-    // //Start Button
-    // $(".start-button").on("click", function () {
-    //     //hides Start Button
-    //     $(".start-button").hide();
-    //     //Shows timer
-    //     $(".timerText").show();
-    //     //Timer counts down from 30 to 0 in increments of 1 second
-    //     var timeleft = 30;
-    //     var questionTimer = setInterval(function () {
-    //         timeleft--;
-    //         document.getElementById("countdowntimer").textContent = timeleft;
-    //         if (timeleft == 0) {
-    //             $("#question").html("Out of Time!")
-    //             hidesChoices();
-    //         }
-    //         if (timeleft <= 0)
-    //             clearInterval(questionTimer);
-    //     }, 1000);
-
-
-    //     //Diplays a question
-    //     Question1();
-    //     blueBrotherAnswers();
 
 
 
 
-    // });
+    // function timer(){
+    var number = 30;
+    var intervalId;
+
     $(".start-button").on("click", function () {
         //   hides Start Button
         $(".start-button").hide();
         //Shows timer
         $(".timerText").show();
-        var timeleft = 30;
-        var questionTimer = setInterval(function () {
-            timeleft--;
-            document.getElementById("countdowntimer").textContent = timeleft;
-            if (timeleft <= 0)
-                clearInterval(questionTimer);
-        }, 1000);
-        //look at week 5 interval & stopwatch 
-    for (const key in questions) {
-        Q(questions[key])
-        if(timeleft == 0){
-            alert("you lose");
-        }
+        timer();
+        displayQuestions();
+
+        // for (const key in questions) {
+        // Q(questions[key]);
+        // choiceClicks();
     
 
-        //user ansers
-        //display to the user quesionz[key].correct
+
+
+
+        // 
+    });
+function displayQuestions() {
+    for ( var i = 0; i < questions.length; i++) {
+        if(!questions[i].answered){
+            $("#question").html(questions[i].text);
+            $("#answerA").html(questions[i].correct);
+            $("#answerB").html(questions[i].wrongOne);
+            $("#answerC").html(questions[i].wrongThree);
+            $("#answerD").html(questions[i].wrongTwo); 
+        }
+        questions[i].answered = true;
+        console.log( questions[i].image );
+        currentQuestion = questions[i];
+
+                break;
     }
-});
+    choiceClicks();
+}
 
-
+    //Functions displays questions and choices
     function Q(obj) {
-        $("#question").html(obj.text);
-        $("#answerA").html(obj.correct);
-        $("#answerB").html(obj.wrongOne);
-        $("#answerC").html(obj.wrongThree);
-        $("#answerD").html(obj.wrongTwo);
-        $("#answerA").on("click", function(){
-            console.log("clickworks")
+     
+    }
+    //function give clickability to choices
+    function choiceClicks() {
+        $("#answerA").on("click", function () {
+            stopTimer();
+            correct();
+            hidesChoices();
+            $('#gif').html(currentQuestion.image);
+            
         });
-        $("#answerB").on("click", function(){
-            console.log("clickworks")
+        $("#answerB").on("click", function () {
+            stopTimer();
+            hidesChoices();
+            nope();
+            $("#correctAnswer").html("The correct answer was: " + currentQuestion.correct);
+            $('#gif').html(currentQuestion.image);
         });
-        $("#answerC").on("click", function(){
-            console.log("clickworks")
+        $("#answerC").on("click", function () {
+            stopTimer();
+            hidesChoices();
+            nope();
+            $("#correctAnswer").html("The correct answer was: " + currentQuestion.correct);
+            $('#gif').html(currentQuestion.image);
         });
 
-        $("#answerD").on("click", function(){
-            console.log("clickworks")
+        $("#answerD").on("click", function () {
+            stopTimer();
+            hidesChoices();
+            nope();
+            $("#correctAnswer").html("The correct answer was: " + currentQuestion.correct);
+            $('#gif').html(currentQuestion.image);
         });
-        
-        
+
+
+    }
+    //function runs timer
+    function timer() {
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        function decrement() {
+            number--;
+            $("#countdowntimer").html(number);
+            if (number === 0) {
+                stopTimer();
+                outOfTime();
+                hidesChoices();
+                $('#gif').html(currentQuestion.image);
+            }
+        }
+        run();
     }
 
+    //function stops timer
+    function stopTimer() {
+        clearInterval(intervalId);
+    }
 
-
-
-    // //functino that displays question 1
-    // function Question1() {
-    //     $("#question").html(questions[0]);
-    //     $("#answerA").html(bluesBrothersOptions[0]);
-    //     $("#answerB").html(bluesBrothersOptions[1]);
-    //     $("#answerC").html(bluesBrothersOptions[2]);
-    //     $("#answerD").html(bluesBrothersOptions[3]);
-    // }
 
     //function that hides choices
     function hidesChoices() {
@@ -139,14 +172,15 @@ $(document).ready(function () {
         $("#answerC").hide();
         $("#answerD").hide();
     }
-
-    // function blueBrotherAnswers() {
-    //     $("#answerA").on("click", function () {
-    //         hidesChoices();
-    //         $("#question").html("Nope!")
-    //         $("#correctAnswer").html("The correct answer was: " + bluesBrothersOptions[3]);
-
-    //     });
-    // }
+    //function displays Nope if wrong anwser is selected
+    function nope() {
+        $("#question").html("Nope!");
+    }
+    function correct(){
+        $("#question").html("Correct!");
+    }
+    function outOfTime(){
+        $("#question").html("Out of Time!");
+    }
 
 });
